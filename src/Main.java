9626af8f4f84be6040b1f2498ee2898b7e91f73f
Nurcan Kurt <112586484+nurcankurt@users.noreply.github.com>
2024-03-22 05:05:12 +0000

    Initial commit.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,57 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String str = "abcdefab";
+		char[] chars = str.toCharArray();
+        lz77(chars);
+       
     }
-}
+    static void lz77(char[] chars){
+        List<Tuple> tupleList = new ArrayList<Tuple>();
+
+        
+        int offset = 0;
+        int length = 0;
+
+        int searchBufferIndex = 0;
+        int seperatorIndex = 1;
+        int lookAheadBufferIndex = 1;
+        String match = null;
+
+        while (searchBufferIndex > 0) {
+
+            if (chars[searchBufferIndex] == chars[lookAheadBufferIndex]) {
+                length++;
+                match = match + chars[searchBufferIndex];
+                while (searchBufferIndex < seperatorIndex) {
+                    searchBufferIndex++;
+                    lookAheadBufferIndex++;
+                    if (chars[searchBufferIndex] == chars[lookAheadBufferIndex]) {
+                        match = match + chars[searchBufferIndex];
+                    }
+                    else
+                    {
+                        Tuple tuple  =new Tuple(offset, length, match);
+                        tupleList.add(tuple);
+                        
+
+                    }
+                    
+                }    
+                
+            }else
+            {
+                offset++;
+                searchBufferIndex--;
+  
+            }
+            
+        }
+
+
+
+    }
+
+} 
---
+++ src/Tuple.java
@@ -0,0 +1,13 @@
+public class Tuple {
+    int offset;
+    int length;
+    String codeword;
+
+
+    public Tuple(int offset, int length, String codeword){
+        this.offset = offset;
+        this.length = length;
+        this.codeword = codeword;
+    }
+    
+}
