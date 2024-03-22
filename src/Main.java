8c900306a14dea114904a99d441dbc0aecc23f62
Nurcan Kurt <112586484+nurcankurt@users.noreply.github.com>
2024-03-22 08:14:57 +0000

    Add codeword

--- src/Main.java
+++ src/Main.java
@@ -3,55 +3,69 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        String str = "abcdefab";
+        String str = "abcbbcbaaaaaa";
 		char[] chars = str.toCharArray();
         lz77(chars);
        
     }
-    static void lz77(char[] chars){
+    static void lz77(char[] chars) {
         List<Tuple> tupleList = new ArrayList<Tuple>();
-
-        
-        int offset = 0;
-        int length = 0;
-
+    
         int searchBufferIndex = 0;
-        int seperatorIndex = 1;
         int lookAheadBufferIndex = 1;
-        String match = null;
-
-        while (searchBufferIndex > 0) {
-
-            if (chars[searchBufferIndex] == chars[lookAheadBufferIndex]) {
-                length++;
-                match = match + chars[searchBufferIndex];
-                while (searchBufferIndex < seperatorIndex) {
-                    searchBufferIndex++;
-                    lookAheadBufferIndex++;
-                    if (chars[searchBufferIndex] == chars[lookAheadBufferIndex]) {
-                        match = match + chars[searchBufferIndex];
-                    }
-                    else
-                    {
-                        Tuple tuple  =new Tuple(offset, length, match);
-                        tupleList.add(tuple);
-                        
-
-                    }
-                    
-                }    
-                
-            }else
-            {
-                offset++;
-                searchBufferIndex--;
-  
+    
+        while (lookAheadBufferIndex < chars.length) {
+            int offset = 0;
+            int length = 0;
+            String match = null;
+            char codeword = ' ';
+    
+            for (int i = searchBufferIndex; i >= 0; i--) {
+                int j = lookAheadBufferIndex;
+                while (j < chars.length && chars[i] == chars[j] && length < j - i) {
+                    length++;
+                    match = new String(chars, i, length);
+                    offset = j - i;
+                    j++;
+                    i++;
+                    codeword = chars[j];
+                }
+                if (length == j - i) {
+                    offset = searchBufferIndex - i;
+                }
             }
-            
+    
+            if (offset == 0) {
+                tupleList.add(new Tuple(0, 0, chars[lookAheadBufferIndex]));
+                searchBufferIndex++;
+                lookAheadBufferIndex++;
+            } else {
+                tupleList.add(new Tuple(offset, length, codeword));
+                searchBufferIndex += length;
+                lookAheadBufferIndex += length;
+            }
         }
-
-
-
+    
+        // Print the tuples
+        for (Tuple tuple : tupleList) {
+            System.out.println(tuple);
+        }
     }
-
+    
+    static class Tuple {
+        int offset;
+        int length;
+        char nextSymbol;
+    
+        public Tuple(int offset, int length, char nextSymbol) {
+            this.offset = offset;
+            this.length = length;
+            this.nextSymbol = nextSymbol;
+        }
+    
+        @Override
+        public String toString() {
+            return "(" + offset + ", " + length + ", " + nextSymbol + ")";
+        }
+    }
 } 
